1a8a7a154b486ef0c5b846f3a2dc584f71ed2d60
greyson <ouyangguanling@gmail.com>
2021-04-28 15:18:36 +0800

    导入文件

--- src/components/common/interfacceConfig.vue
+++ src/components/common/interfacceConfig.vue
@@ -4,7 +4,8 @@
     <div style="margin-bottom: 20px">
       <el-button @click="dialogTableVisible = true"
                  type="primary">新增</el-button>
-      <el-button type="primary">导入</el-button>
+      <el-button type="primary"
+                 @click="uploadExcelTabVisiable = true">导入</el-button>
       <el-button type="primary">导出</el-button>
     </div>
     <el-table :data="tableData"
@@ -66,6 +67,31 @@
         </el-form-item>
       </el-form>
     </el-dialog>
+
+    <el-dialog title="上传Excel"
+               :visible.sync="uploadExcelTabVisiable"
+               :close-on-click-modal="false">
+      <el-upload drag
+                 class="upload-excel"
+                 :auto-upload="false"
+                 :action="UploadUrl()"
+                 :limit=limitNum
+                 accept=".xlsx"
+                 :on-change="fileChange"
+                 :on-exceed="exceedFile"
+                 :on-success="handleSuccess"
+                 :on-error="handleError"
+                 :file-list="fileList">
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      </el-upload>
+      <br />
+      <el-button size="small"
+                 type="primary"
+                 @click="uploadFile">立即上传</el-button>
+      <el-button size="small"
+                 @click="uploadExcelTabVisiable = false">取消</el-button>
+    </el-dialog>
   </el-card>
 
 </template>
@@ -73,10 +99,18 @@
 <script>
 import axios from 'axios';
 
+
 export default {
-  name: 'FormDialog',
+  name: 'interfacceConfig',
+  components: {
+  },
   data () {
     return {
+      // 上传excel文件列表
+      limitNum: 1,
+      fileList: [],
+      // excel组件可视
+      uploadExcelTabVisiable: false,
       dialogTableVisible: false, // 添加用户弹框
       // 控制是否显示
       dialogFormVisible: false,
@@ -122,6 +156,65 @@ export default {
 
   },
   methods: {
+    // 文件状态改变时的钩子
+    fileChange (file, fileList) {
+      console.log(file.raw);
+      this.fileList.push(file.raw);
+      console.log(this.fileList);
+    },
+    // 上传文件之前的钩子, 参数为上传的文件,若返回 false 或者返回 Promise 且被 reject，则停止上传
+    beforeUploadFile (file) {
+      console.log('before upload');
+      console.log(file);
+      let extension = file.name.substring(file.name.lastIndexOf('.') + 1);
+      let size = file.size / 1024 / 1024;
+      if (extension !== 'xlsx') {
+        this.$message.warning('只能上传后缀是.xlsx的文件');
+      }
+      if (size > 10) {
+        this.$message.warning('文件大小不得超过10M');
+      }
+    },
+    // 文件超出个数限制时的钩子
+    exceedFile (files, fileList) {
+      this.$message.warning(`只能选择 ${this.limitNum} 个文件，当前共选择了 ${files.length + fileList.length} 个`);
+    },
+    // 文件上传成功时的钩子
+    handleSuccess (res, file, fileList) {
+      this.$message.success('文件上传成功');
+    },
+    // 文件上传失败时的钩子
+    handleError (err, file, fileList) {
+      this.$message.error('文件上传失败');
+    },
+    UploadUrl: function () {
+      // 因为action参数是必填项，我们使用二次确认进行文件上传时，直接填上传文件的url会因为没有参数导致api报404，所以这里将action设置为一个返回为空的方法就行，避免抛错
+      return ""
+    },
+    uploadFile () {
+      if (this.fileList.length === 0) {
+        this.$message.warning('请上传文件');
+      } else {
+        let form = new FormData();
+        form.append('file', this.fileList);
+        var headers = {
+            'Content-type': 'multipart/form-data'
+          }
+        axios.post("/api/excelUtil/importExcel",form, headers);
+        // this.$axios({
+        //   method: "post",
+        //   url: "/api/excelUtil/importExcel",
+        //   headers: {
+        //     'Content-type': 'multipart/form-data'
+        //   },
+        //   data: form
+        // }).then(
+        //   res => {
+
+        //   }, err => {
+        //   });
+      }
+    },
     isRequestSuccess (data) {
       var res = JSON.stringify(data.data);
       var result = JSON.parse(res);
