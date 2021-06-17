b77cc9d7f048b17f8fe7d6ab5f003c16307d2cc2
greyson <ouyangguanling@gmail.com>
2021-06-17 17:05:02 +0800

    解决动态获取url的bug

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,9 +1,9 @@
 /* 通过 qs 模块处理请求数据*/
 import axios from "axios";
-import qs from "qs";
 
-// axios.defaults.baseURL = "http://10.18.2.64:7090";
-axios.defaults.baseURL = "http://127.0.0.1:7090";
+let protocol = window.location.protocol; //协议
+let host = window.location.hostname; //主机
+axios.defaults.baseURL = protocol + "//" + host  +":7090";
 axios.defaults.withCredentials = true; // 若跨域请求需要带 cookie 身份识别
 axios.defaults.headers.post["Access-Control-Allow-Origin-Type"] = "*"; // 允许跨域
 // axios.defaults.headers.post["Content-Type"] =
